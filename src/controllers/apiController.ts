d8999dcbebd06490e25bdd7278bc7e308e88af6d
MirandaDiego <diegoflam12@gmail.com>
2023-07-21 12:19:46 -0300

    atualizando

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -78,3 +78,21 @@ export const randomPhrase = async ( req:Request, res:Response)=>{
         res.json({ error: 'Não à frases cadastradas'})
     }
 }
+
+export const uploadFile = async (req: Request, res: Response)=>{  
+    //para um arquivo:
+    //console.log( req.file)
+
+    //para dois arquivos:
+    //console.log( req.files)
+
+    //para os arquivos diferentes:
+/*const files = req.files as { [fieldname: string]: Express.Multer.File[]};
+    console.log("AVATAR", files.avatar)
+    console.log("GALLERY", files.gallery) */
+
+    console.log("FILE", req.file)
+    console.log("FILES", req.files)
+
+    res.json({})
+}
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,33 @@
 //PADRAO REST
 import { Router } from "express";
+import multer from "multer";
 import * as ApiController from '../controllers/apiController';
+
+
+
+//config. simples: para  onde vai o arquivo:
+
+/*const upload = multer({
+    dest: './tmp'
+
+}) */
+
+
+const storageConfig  = multer.diskStorage({
+    destination: (req, file, cb) => {
+        //destino do arquivo:
+        cb(null, './tmp')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname+'-'+Date.now())
+    }
+}); 
+
+
+const upload = multer({
+    storage: storageConfig
+}); 
+
 
 
 const router = Router()
@@ -15,6 +42,22 @@ router.get('/frase/aleatoria', ApiController.randomPhrase)
 router.get('/frase/:id', ApiController.getPhrases)
 router.put('/frase/:id', ApiController.updatePhrase)
 router.delete('/frase/:id', ApiController.deletePhrase);
+
+//ROTA,  CALLBACK DE UPLOAD, ROTA
+//receber um arquivo somente:
+//router.post('/upload', upload.single('avatar'), ApiController.uploadFile);
+
+//receber dois arquivos:
+//router.post('/upload', upload.array('avatares', 2), ApiController.uploadFile);
+
+
+//Recebimentos de varios arquivos diferentes juntos:
+ /*router.post('/upload', upload.fields([
+    {name: 'avatar', maxCount: 1},
+    {name: 'gallery', maxCount: 3}
+]), ApiController.uploadFile) */
+
+router.post('/upload', upload.single('avatar'), ApiController.uploadFile);
 
 
 
